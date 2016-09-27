8082b84ae6149155001ebc585d86c1ca0d71537f
SHOHRUX <shohrux.haqberdiyev@gmail.com>
2016-09-28 04:10:39 +0500

    I upload new files that helpers and libraryies

--- application/config/config.php
+++ application/config/config.php
@@ -65,7 +65,7 @@ $config['uri_protocol']	= 'AUTO';
 | http://codeigniter.com/user_guide/general/urls.html
 */
 
-$config['url_suffix'] = '';
+$config['url_suffix'] = '.html';
 
 /*
 |--------------------------------------------------------------------------
