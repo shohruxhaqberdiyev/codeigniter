5b10413fdbae5b35f665f9a478711ebf54892daa
SHOHRUX <shohrux.haqberdiyev@gmail.com>
2016-09-28 04:19:04 +0500

    I upload new files that helpers and libraryies

---
+++ application/controllers/blog.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SHOHRUX
+ * Date: 28.09.16
+ * Time: 4:18
+ */
+
+class Blog extends CI_Controller {
+
+    public function index()
+    {
+        echo 'Hello World!';
+    }
+}
